e16b2a7de1d20843262e1343682e0ddafba9825c
lynsiearagon <lynsie.aragon@gmail.com>
2022-11-16 16:51:37 -0500

    Boad complete, Player Complete

--- battleship_project/battleship_project/lib/board.rb
+++ battleship_project/battleship_project/lib/board.rb
@@ -1,5 +1,9 @@
 class Board
 
+    def self.print_grid(grid)
+        grid.each { |subarr| puts subarr.join(" ") }
+    end
+
     attr_reader :size
 
     def initialize(n)
@@ -18,6 +22,56 @@ class Board
     end
 
     def num_ships
+        count = 0
+        @grid.each do |subarr| 
+            subarr.each { |el| count += 1 if el == :S}
+        end
+        count
+    end
+
+    def attack(position)
+        if self[position] == :S
+            self[position] = :H
+            puts 'you sunk my battleship!'
+            return true
+        else
+            self[position] = :X 
+            return false 
+        end
+    end
         
-  
+    def place_random_ships
+        max_ships = size / 4
+        ship_count = 0
+
+        until ship_count == max_ships
+            row = rand(0...@grid.length)
+            col = rand(0...@grid.length)
+            if @grid[row][col] != :S
+                @grid[row][col] = :S
+                ship_count += 1
+            end
+        end
+    end
+
+    def hidden_ships_grid
+        @grid.map do |subarr|
+            subarr.map do |ele|
+                if ele == :S 
+                    :N
+                else 
+                    ele
+                end
+            end
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(hidden_ships_grid)
+    end
+    
 end
